e2c52f9c3a688ab2346198ebddef8d91951031df
gamliel <gamlielchemla@gmail.com>
2020-11-01 12:43:56 +0200

    first commit

---
+++ test1.js
@@ -0,0 +1,11 @@
+"use strict";
+// // let myvariable ;
+var user1 = {
+    name: "gamlel",
+    prenom: "chemla",
+    age: 33
+};
+function hello(person) {
+    console.log("bonjour a " + person.name + "le king");
+}
+hello(user1);
